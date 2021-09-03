af8fce54c59dbd0db130f2702a00a2af7cfc4fd5
Ali Hashemi <alihashemi.ee@gmail.com>
2021-09-03 14:53:58 +0200

    check the test for iterative typeII-L2: 6 passed

--- bsi_zoo/tests/test_estimators.py
+++ bsi_zoo/tests/test_estimators.py
@@ -1,5 +1,5 @@
 import numpy as np
-from bsi_zoo.estimators import reweighted_lasso, iterative_L1, iterative_L2, iterative_sqrt, iterative_L1_typeII
+from bsi_zoo.estimators import reweighted_lasso, iterative_L1, iterative_L2, iterative_sqrt, iterative_L1_typeII, iterative_L2_typeII
 
 def _generate_data(n_sensors, n_times, n_sources, nnz):
     rng = np.random.RandomState(42)
@@ -57,3 +57,13 @@ def test_iterative_L1_typeII():
 
     np.testing.assert_array_equal(x != 0, x_hat != 0)
     np.testing.assert_allclose(x, x_hat, atol=1e-1, rtol=5e-1)
+
+
+def test_iterative_L2_typeII():
+    y, L, x, cov = _generate_data(n_sensors=50, n_times=1, n_sources=200,
+                                  nnz=1)
+    x_hat = iterative_L2_typeII(L, y[:, 0], cov, alpha=0.1)
+    x = x[:, 0]
+
+    np.testing.assert_array_equal(x != 0, x_hat != 0)
+    np.testing.assert_allclose(x, x_hat, atol=1e1, rtol=1e-1)
