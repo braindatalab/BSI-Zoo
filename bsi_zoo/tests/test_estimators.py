b8ab7123eb48941d9bb25d7818bace6ae2ed43cf
Ali Hashemi <alihashemi.ee@gmail.com>
2021-09-09 08:53:59 +0200

    cleaning the test function

--- bsi_zoo/tests/test_estimators.py
+++ bsi_zoo/tests/test_estimators.py
@@ -30,7 +30,6 @@ def _generate_data(n_sensors, n_times, n_sources, nnz):
         cov = rng.randn(n_sensors, n_sensors)
         cov = 1e-3 * (cov @ cov.T)
         # cov = 1e-3 * (cov @ cov.T) / n_times ## devided by the number of time samples for better scalinggit 
-        
     noise = rng.multivariate_normal(np.zeros(n_sensors), cov, size=n_times).T
     y += noise
     return y, L, x, cov
@@ -44,21 +43,8 @@ def _generate_data(n_sensors, n_times, n_sources, nnz):
         (iterative_L2_typeII, 0.3, 1e-1, 1e-1, 'full'),
     ]
 )
-# def test_estimator(solver, alpha, rtol, atol, cov_type):
-#     y, L, x, cov = _generate_data(n_sensors=50, n_times=1, n_sources=200, nnz=1)
-#     if cov_type == 'diag':
-#         whitener = linalg.inv(linalg.sqrtm(cov))
-#         L = whitener @ L
-#         y = whitener @ y
-#         x_hat = solver(L, y[:, 0], alpha=alpha)
-#     else:
-#         x_hat = solver(L, y[:, 0], cov, alpha=alpha)
-#     x = x[:, 0]
-#     np.testing.assert_array_equal(x != 0, x_hat != 0)
-#     np.testing.assert_allclose(x, x_hat, rtol=rtol, atol=atol)
-
 def test_estimator(solver, alpha, rtol, atol, cov_type):
-    y, L, x, cov = _generate_data(n_sensors=50, n_times=10, n_sources=200, nnz=1)
+    y, L, x, cov = _generate_data(n_sensors=50, n_times=20, n_sources=200, nnz=1)
     if cov_type == 'diag':
         whitener = linalg.inv(linalg.sqrtm(cov))
         L = whitener @ L
@@ -66,21 +52,5 @@ def test_estimator(solver, alpha, rtol, atol, cov_type):
         x_hat = solver(L, y, alpha=alpha)
     else:
         x_hat = solver(L, y, cov, alpha=alpha)
-    # x = x[:, 0]
-    # x = x.T
     np.testing.assert_array_equal(x != 0, x_hat != 0)
     np.testing.assert_allclose(x, x_hat, rtol=rtol, atol=atol)
-
-# # # Test the performance for single measuremnt vector (SMV) case
-# def test_estimator(solver, alpha, rtol, atol, cov_type):
-#     y, L, x, cov = _generate_data(n_sensors=50, n_times=1, n_sources=200, nnz=1)
-#     if cov_type == 'diag':
-#         whitener = linalg.inv(linalg.sqrtm(cov))
-#         L = whitener @ L
-#         y = whitener @ y
-#         x_hat = solver(L, y[:, 0], alpha=alpha)
-#     else:
-#         x_hat = solver(L, y[:, 0], cov, alpha=alpha)
-#     x = x[:, 0]
-#     np.testing.assert_array_equal(x != 0, x_hat != 0)
-#     np.testing.assert_allclose(x, x_hat, rtol=rtol, atol=atol)
