780e565a3e7b423f8d913c17d4702c50d5404f37
Ali Hashemi <alihashemi.ee@gmail.com>
2021-09-09 11:10:24 +0200

    fix conflict with MMV branch + merge

--- bsi_zoo/tests/test_estimators.py
+++ bsi_zoo/tests/test_estimators.py
@@ -3,7 +3,6 @@ from scipy import linalg
 import pytest
 import pdb 
 from bsi_zoo.estimators import (
-    reweighted_lasso,
     iterative_L1,
     iterative_L2,
     iterative_sqrt,
@@ -18,41 +17,33 @@ def _generate_data(n_sensors, n_times, n_sources, nnz):
     x[:nnz] = rng.randn(nnz, n_times)
     L = rng.randn(n_sensors, n_sources)  # TODO: add orientation support
     y = L @ x
-    cov = rng.randn(n_sensors, n_sensors)
-    cov = 1e-3 * (cov @ cov.T)
-    # cov = 1e-3 * (cov @ cov.T) / n_times ## devided by the number of time samples for better scalinggit 
-    # cov = np.diag(np.diag(cov))
-    # cov = 1e-2 * np.diag(np.ones(n_sensors))
+    cov_type = 'full'
+    if cov_type == 'diag':
+        ## initialization of the noise covariance matrix with a random diagonal matrix 
+        cov = rng.randn(n_sensors, n_sensors)
+        cov = 1e-3 * (cov @ cov.T)
+        cov = np.diag(np.diag(cov))
+        ## initialization of the noise covariance with an identity matrix
+        cov = 1e-2 * np.diag(np.ones(n_sensors))
+    else:
+        cov = rng.randn(n_sensors, n_sensors)
+        cov = 1e-3 * (cov @ cov.T)
+        # cov = 1e-3 * (cov @ cov.T) / n_times ## devided by the number of time samples for better scalinggit 
     noise = rng.multivariate_normal(np.zeros(n_sensors), cov, size=n_times).T
     y += noise
     return y, L, x, cov
 
-
 @pytest.mark.parametrize(
     "solver,alpha,rtol,atol,cov_type", [
-        (reweighted_lasso, 0.1, 1e-1, 0, 'diag'),
         (iterative_L1, 0.1, 1e-1, 5e-1, 'diag'),
-        (iterative_L2, 0.01, 1e-1, 0, 'diag'),
-        (iterative_sqrt, 0.1, 1e-1, 0, 'diag'),
+        (iterative_L2, 0.2, 1e-1, 5e-1, 'diag'),
+        (iterative_sqrt, 0.1, 1e-1, 5e-1, 'diag'),
         (iterative_L1_typeII, 0.1, 1e-1, 5e-1, 'full'),
-        (iterative_L2_typeII, 0.1, 1e1, 1e-1, 'full'),
+        (iterative_L2_typeII, 0.2, 1e-1, 1e-1, 'full'),
     ]
 )
-# def test_estimator(solver, alpha, rtol, atol, cov_type):
-#     y, L, x, cov = _generate_data(n_sensors=50, n_times=1, n_sources=200, nnz=1)
-#     if cov_type == 'diag':
-#         whitener = linalg.inv(linalg.sqrtm(cov))
-#         L = whitener @ L
-#         y = whitener @ y
-#         x_hat = solver(L, y[:, 0], alpha=alpha)
-#     else:
-#         x_hat = solver(L, y[:, 0], cov, alpha=alpha)
-#     x = x[:, 0]
-#     np.testing.assert_array_equal(x != 0, x_hat != 0)
-#     np.testing.assert_allclose(x, x_hat, rtol=rtol, atol=atol)
-
 def test_estimator(solver, alpha, rtol, atol, cov_type):
-    y, L, x, cov = _generate_data(n_sensors=50, n_times=1, n_sources=200, nnz=1)
+    y, L, x, cov = _generate_data(n_sensors=50, n_times=10, n_sources=200, nnz=1)
     if cov_type == 'diag':
         whitener = linalg.inv(linalg.sqrtm(cov))
         L = whitener @ L
@@ -60,21 +51,5 @@ def test_estimator(solver, alpha, rtol, atol, cov_type):
         x_hat = solver(L, y, alpha=alpha)
     else:
         x_hat = solver(L, y, cov, alpha=alpha)
-    # x = x[:, 0]
-    x = x.T
     np.testing.assert_array_equal(x != 0, x_hat != 0)
     np.testing.assert_allclose(x, x_hat, rtol=rtol, atol=atol)
-
-# # # Test the performance for single measuremnt vector (SMV) case
-# def test_estimator(solver, alpha, rtol, atol, cov_type):
-#     y, L, x, cov = _generate_data(n_sensors=50, n_times=1, n_sources=200, nnz=1)
-#     if cov_type == 'diag':
-#         whitener = linalg.inv(linalg.sqrtm(cov))
-#         L = whitener @ L
-#         y = whitener @ y
-#         x_hat = solver(L, y[:, 0], alpha=alpha)
-#     else:
-#         x_hat = solver(L, y[:, 0], cov, alpha=alpha)
-#     x = x[:, 0]
-#     np.testing.assert_array_equal(x != 0, x_hat != 0)
-#     np.testing.assert_allclose(x, x_hat, rtol=rtol, atol=atol)
