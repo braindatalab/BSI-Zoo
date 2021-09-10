1d7ef0711155ef8df260c10049677a306ed454e9
Ali Hashemi <alihashemi.ee@gmail.com>
2021-09-10 12:03:15 +0200

    add gamma_map and champagne

--- bsi_zoo/tests/test_estimators.py
+++ bsi_zoo/tests/test_estimators.py
@@ -10,6 +10,7 @@ from bsi_zoo.estimators import (
     iterative_L1_typeII,
     iterative_L2_typeII,
     gamma_map,
+    champagne,
 )
 
 
@@ -43,7 +44,8 @@ def _generate_data(n_sensors, n_times, n_sources, nnz):
         (iterative_sqrt, 0.1, 1e-1, 5e-1, 'diag'),
         (iterative_L1_typeII, 0.1, 1e-1, 5e-1, 'full'),
         (iterative_L2_typeII, 0.2, 1e-1, 1e-1, 'full'),
-        (gamma_map, 0.2, 1e-1, 1e-1, 'full'),
+        (gamma_map, 0.2, 1e-1, 5e-1, 'full'),
+        (champagne, 0.2, 1e-1, 5e-1, 'full'),
     ]
 )
 def test_estimator(solver, alpha, rtol, atol, cov_type):
