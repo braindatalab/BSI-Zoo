085c92ea0cc7aa36b40eff5c62fabadc4c7e7ab8
anujanegi <anujanegi1@gmail.com>
2021-10-05 11:53:24 +0200

    added tests

--- bsi_zoo/tests/test_estimators.py
+++ bsi_zoo/tests/test_estimators.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import linalg
 import pytest
+from skimage.feature import peak_local_max
 
 from bsi_zoo.estimators import (
     iterative_L1,
@@ -16,7 +17,7 @@ def _generate_data(n_sensors, n_times, n_sources, nnz, cov_type, path_to_leadfie
     rng = np.random.RandomState(42)
     if path_to_leadfield is not None:
         lead_field = np.load(path_to_leadfield, allow_pickle=True)
-        L = lead_field['lead_field']
+        L = lead_field["lead_field"]
         n_sensors, n_sources = L.shape
     else:
         L = rng.randn(n_sensors, n_sources)  # TODO: add orientation support
@@ -41,18 +42,18 @@ def _generate_data(n_sensors, n_times, n_sources, nnz, cov_type, path_to_leadfie
         cov = 1e-3 * (cov @ cov.T)
         # cov = 1e-3 * (cov @ cov.T) / n_times ## devided by the number of time samples for better scaling
     noise = rng.multivariate_normal(np.zeros(n_sensors), cov, size=n_times).T
+    # snr = 20 * np.log(np.linalg.norm(y, 'fro') / np.linalg.norm(noise, 'fro'))
     y += noise
     if n_times == 1:
         y = y[:, 0]
         x = x[:, 0]
-    return y, L, x, cov
+
+    return y, L, x, cov, noise
 
 
+@pytest.mark.parametrize("n_times", [1, 10])
 @pytest.mark.parametrize(
-    "n_times", [1, 10]
-)
-@pytest.mark.parametrize(
-    "path_to_leadfield", [None, 'bsi_zoo/tests/data/lead_field.npz']
+    "path_to_leadfield", [None, "bsi_zoo/tests/data/lead_field.npz"]
 )
 @pytest.mark.parametrize(
     "solver,alpha,rtol,atol,cov_type",
@@ -66,7 +67,14 @@ def _generate_data(n_sensors, n_times, n_sources, nnz, cov_type, path_to_leadfie
     ],
 )
 def test_estimator(n_times, solver, alpha, rtol, atol, cov_type, path_to_leadfield):
-    y, L, x, cov = _generate_data(n_sensors=50, n_times=n_times, n_sources=200, nnz=1, cov_type=cov_type, path_to_leadfield=path_to_leadfield)
+    y, L, x, cov, noise = _generate_data(
+        n_sensors=50,
+        n_times=n_times,
+        n_sources=200,
+        nnz=1,
+        cov_type=cov_type,
+        path_to_leadfield=path_to_leadfield,
+    )
     if cov_type == "diag":
         whitener = linalg.inv(linalg.sqrtm(cov))
         L = whitener @ L
@@ -74,5 +82,29 @@ def test_estimator(n_times, solver, alpha, rtol, atol, cov_type, path_to_leadfie
         x_hat = solver(L, y, alpha=alpha)
     else:
         x_hat = solver(L, y, cov, alpha=alpha)
-    np.testing.assert_array_equal(x != 0, x_hat != 0)
-    np.testing.assert_allclose(x, x_hat, rtol=rtol, atol=atol)
+
+    noise_hat = y - (L @ x_hat)
+    if n_times > 1:
+        np.testing.assert_array_less(
+            np.linalg.norm(
+                peak_local_max(np.linalg.norm(x, axis=0), num_peaks=1)
+                - peak_local_max(np.linalg.norm(x_hat, axis=0), num_peaks=1)
+            ),
+            1.1,
+        )
+    else:
+        np.testing.assert_array_less(
+            np.linalg.norm(
+                peak_local_max(x, num_peaks=1) - peak_local_max(x_hat, num_peaks=1)
+            ),
+            1.1,
+        )
+
+    if path_to_leadfield is None:
+        np.testing.assert_array_equal(x != 0, x_hat != 0)
+        np.testing.assert_allclose(x, x_hat, rtol=rtol, atol=atol)
+
+    # residual error check
+    np.testing.assert_allclose(
+        noise, noise_hat[:, np.newaxis], rtol=1, atol=5
+    )  # TODO: decide threshold
