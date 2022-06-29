a4b42c1d2c96d3c014e233f3184da50c4e7d4a5e
anujanegi <anujanegi1@gmail.com>
2022-06-29 18:00:40 +0200

    profile format modified from json to pandas df

--- bsi_zoo/benchmark.py
+++ bsi_zoo/benchmark.py
@@ -4,6 +4,8 @@ from bsi_zoo.metrics import euclidean_distance, mse
 from bsi_zoo.config import get_leadfield_path
 import json
 from scipy import linalg
+import random
+import pandas as pd
 
 
 class Benchmark:
@@ -24,18 +26,21 @@ class Benchmark:
         self.save_profile = save_profile
 
     def benchmark(self, nruns=2):
-        profile = {
-            arg_to_benchmark: {} for arg_to_benchmark in self.data_agrs_to_benchmark
-        }
+        profile = {}
+        seeds = random.sample(range(1, 100), nruns)
+
         for arg_to_benchmark in self.data_agrs_to_benchmark:
             for arg_value in self.data_agrs_to_benchmark[arg_to_benchmark]:
                 self.data_agrs[arg_to_benchmark] = arg_value
-                print(data_agrs)
-                y, L, x, cov, _ = get_data(**self.data_agrs)
 
-                profile[arg_to_benchmark][arg_value] = {}
-                profile_to_store = profile[arg_to_benchmark][arg_value]
-                for _ in range(nruns):
+                print("Benchmarking this data...")
+                print(data_agrs)
+
+                store_metrics = {m.__name__: [] for m in self.metrics}
+                for i, _ in enumerate(range(nruns)):
+                    # get data
+                    y, L, x, cov, _ = get_data(**self.data_agrs, seed=seeds[i])
+
                     # estimate x_hat
                     if self.data_agrs["cov_type"] == "diag":
                         whitener = linalg.inv(linalg.sqrtm(cov))
@@ -53,50 +58,58 @@ class Benchmark:
                             orientation_type=self.data_agrs["orientation_type"],
                             nnz=self.data_agrs["nnz"],
                         )
-                        if metric.__name__ in profile_to_store:
-                            profile_to_store[metric.__name__].append(metric_score)
-                        else:
-                            profile_to_store[metric.__name__] = [metric_score]
+                        store_metrics[metric.__name__].append(metric_score)
+
+                profile["%s=%.2f" % (arg_to_benchmark, arg_value)] = sum(
+                    list(store_metrics.values()), []
+                )
 
         if self.save_profile:
-            with open(
-                "bsi_zoo/data/benchmark_data_%s_%s_%s_orient.json"
+
+            indexes = []
+            for m in metrics:
+                indexes += [m.__name__] * nruns
+            df = pd.DataFrame(profile, index=indexes)
+
+            df.to_pickle(
+                "bsi_zoo/data/benchmark_data_%s_%s_%s_orient_nnz_%d.pkl"
                 % (
                     self.subject,
                     self.estimator.__name__,
                     self.data_agrs["orientation_type"],
-                ),
-                "w",
-            ) as fp:
-                json.dump(profile, fp)
-                print(
-                    "Profile saved for subject %s for %s estimator!"
-                    % (self.subject, self.estimator.__name__)
+                    self.data_agrs["nnz"],
                 )
+            )
+
+            print(
+                "Profile saved for subject %s for %s estimator!"
+                % (self.subject, self.estimator.__name__)
+            )
 
         return profile
 
 
 if __name__ == "__main__":
+    # for nnz=2
     subject = "CC120264"
     data_agrs = {
         "n_sensors": 50,
         "n_times": 10,
         "n_sources": 200,
         "n_orient": 3,
-        "nnz": 3,
+        "nnz": 2,
         "cov_type": "full",
         "path_to_leadfield": get_leadfield_path(subject, type="fixed"),
         "orientation_type": "fixed",
         "alpha": 0.99,
     }
 
-    data_agrs_to_benchmark = {"alpha": [0.99, 0.9, 0.8, 0.5], "nnz": [3, 5, 10]}
+    data_agrs_to_benchmark = {"alpha": [0.99, 0.9, 0.8, 0.5]}
     metrics = [euclidean_distance, mse]  # list of metric functions here
 
     benchmark_gamma_map = Benchmark(
         gamma_map, subject, metrics, data_agrs, data_agrs_to_benchmark
     )
 
-    gamma_map_profile = benchmark_gamma_map.benchmark(nruns=3)
+    gamma_map_profile = benchmark_gamma_map.benchmark(nruns=5)
     print(gamma_map_profile)
