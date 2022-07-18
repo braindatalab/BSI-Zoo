b603dd7d591e46952d6613f234edec62d9b90f73
anujanegi <anujanegi1@gmail.com>
2022-07-18 16:57:46 +0200

    reduced alpha for iterative L1

--- bsi_zoo/tests/test_estimators.py
+++ bsi_zoo/tests/test_estimators.py
@@ -22,7 +22,7 @@ from bsi_zoo.estimators import (
 @pytest.mark.parametrize(
     "solver,alpha,rtol,atol,cov_type",
     [
-        (iterative_L1, 0.1, 1e-1, 5e-1, "diag"),
+        (iterative_L1, 0.01, 1e-1, 5e-1, "diag"),
         (iterative_L2, 0.01, 1e-1, 5e-1, "diag"),
         (iterative_sqrt, 0.1, 1e-1, 5e-1, "diag"),
         (iterative_L1_typeII, 0.1, 1e-1, 5e-1, "full"),
