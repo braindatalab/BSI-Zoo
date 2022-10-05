59e3c3bbff5fa2388731e9ab91b86eb26cefd906
Ali Hashemi <alihashemi.ee@gmail.com>
2022-10-05 22:24:20 +0200

    correct the location of lead fileds

--- bsi_zoo/config.py
+++ bsi_zoo/config.py
@@ -2,8 +2,10 @@ def get_leadfield_path(subject, type=""):
     if subject is not None:
         if type == "free":
             return "bsi_zoo/tests/data/lead_field_free_%s.npz" % subject
+            # return "bsi_zoo/tests/data/benchmark_data_%s_free.pkl" % subject
         elif type == "fixed":
             return "bsi_zoo/tests/data/lead_field_%s.npz" % subject
+            # return "bsi_zoo/tests/data/benchmark_data_%s_fixed.pkl" % subject
     else:
         return None
 
