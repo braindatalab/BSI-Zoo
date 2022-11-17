e94a8d0c8c409b56b99d3ddabd8124ba9fc0751a
anujanegi <anujanegi1@gmail.com>
2022-11-17 22:54:58 +0100

    split benchmarking routines

--- bsi_zoo/benchmark.py
+++ bsi_zoo/benchmark.py
@@ -1,6 +1,5 @@
 import numpy as np
 import itertools
-from pathlib import Path
 from scipy import linalg
 import pandas as pd
 from joblib import Memory, Parallel, delayed
@@ -9,17 +8,7 @@ from sklearn.model_selection import ParameterGrid
 
 
 from bsi_zoo.data_generator import get_data
-from bsi_zoo.estimators import (
-    iterative_L1,
-    iterative_L2,
-    iterative_L1_typeII,
-    iterative_L2_typeII,
-    gamma_map,
-    iterative_sqrt,
-    SpatialCVSolver,
-)
-from bsi_zoo.metrics import euclidean_distance, mse, emd, f1
-from bsi_zoo.config import get_leadfield_path
+from bsi_zoo.estimators import SpatialCVSolver, Solver
 
 
 def _run_estimator(
@@ -43,7 +32,7 @@ def _run_estimator(
         whitener = linalg.inv(linalg.sqrtm(cov))
         L = whitener @ L
         y = whitener @ y
-        
+
     # estimate x_hat
     if do_spatial_cv:
         estimator_ = SpatialCVSolver(
@@ -107,7 +96,7 @@ class Benchmark:
         memory=None,
         n_jobs=1,
         do_spatial_cv=False,
-        estimator_extra_params={}
+        estimator_extra_params={},
     ) -> None:
         self.estimator = estimator
         self.subject = subject
@@ -119,6 +108,7 @@ class Benchmark:
         self.n_jobs = n_jobs
         self.do_spatial_cv = do_spatial_cv
         self.estimator_extra_params = estimator_extra_params
+        self.do_spatial_cv = do_spatial_cv
 
     def run(self, nruns=2):
         rng = check_random_state(self.random_state)
@@ -126,7 +116,7 @@ class Benchmark:
 
         estimator = self.memory.cache(self.estimator)
 
-        if do_spatial_cv:
+        if self.do_spatial_cv:
             # dont make param grid for estimator args
             results = Parallel(n_jobs=self.n_jobs)(
                 delayed(_run_estimator)(
@@ -139,7 +129,7 @@ class Benchmark:
                     estimator_name=self.estimator.__name__,
                     memory=self.memory,
                     do_spatial_cv=self.do_spatial_cv,
-                    extra_params=estimator_extra_params
+                    extra_params=self.estimator_extra_params,
                 )
                 for this_data_args, seed in itertools.product(
                     ParameterGrid(self.data_args), seeds
@@ -167,158 +157,3 @@ class Benchmark:
 
         results = pd.DataFrame(results)
         return results
-
-
-if __name__ == "__main__":
-    n_jobs = 20
-    do_spatial_cv = True
-    metrics = [euclidean_distance, mse, emd, f1]  # list of metric functions here
-    nnzs = [1, 2, 3, 5]
-    estimator_alphas = [
-        0.01,
-        0.01544452,
-        0.02385332,
-        0.03684031,
-        0.0568981,
-        0.08787639,
-        0.13572088,
-        0.2096144,
-    ]  # logspaced
-    memory = Memory(".")
-
-    for subject in ["CC120166", "CC120264", "CC120313", "CC120309"]:
-        """ Fixed orientation parameters for the benchmark """
-
-        data_args_I = {
-            "n_sensors": [50],
-            "n_times": [10],
-            "n_sources": [200],
-            "n_orient": [3],
-            "nnz": nnzs,
-            "cov_type": ["diag"],
-            "path_to_leadfield": [get_leadfield_path(subject, type="fixed")],
-            "orientation_type": ["fixed"],
-            "alpha": [0.9, 0.8, 0.6, 0.5, 0.4],  # this is actually SNR
-        }
-
-        data_args_II = {
-            "n_sensors": [50],
-            "n_times": [10],
-            "n_sources": [200],
-            "n_orient": [3],
-            "nnz": nnzs,
-            "cov_type": ["full"],
-            "path_to_leadfield": [get_leadfield_path(subject, type="fixed")],
-            "orientation_type": ["fixed"],
-            "alpha": [0.9, 0.8, 0.6, 0.5, 0.4],  # this is actually SNR
-        }
-
-        estimators = [
-            # (iterative_L1, data_args_I, {"alpha": estimator_alphas}, {}),
-            # (iterative_L2, data_args_I, {"alpha": estimator_alphas}, {}),
-            # (iterative_sqrt, data_args_I, {"alpha": estimator_alphas}, {}),
-            # (iterative_L1_typeII, data_args_II, {"alpha": estimator_alphas}, {}),
-            # (iterative_L2_typeII, data_args_II, {"alpha": estimator_alphas}, {}),
-            (gamma_map, data_args_II, {"alpha": estimator_alphas},  {"update_mode": 1}),
-            (gamma_map, data_args_II, {"alpha": estimator_alphas},  {"update_mode": 2}),
-            (gamma_map, data_args_II, {"alpha": estimator_alphas},  {"update_mode": 3}),
-        ]
-
-        df_results = []
-        for estimator, data_args, estimator_args, estimator_extra_params in estimators:
-            benchmark = Benchmark(
-                estimator,
-                subject,
-                metrics,
-                data_args,
-                estimator_args,
-                random_state=42,
-                memory=memory,
-                n_jobs=n_jobs,
-                do_spatial_cv=do_spatial_cv,
-                estimator_extra_params=estimator_extra_params
-            )
-            results = benchmark.run(nruns=10)
-            df_results.append(results)
-
-        df_results = pd.concat(df_results, axis=0)
-
-        data_path = Path("bsi_zoo/data")
-        data_path.mkdir(exist_ok=True)
-        df_results.to_pickle(
-            data_path
-            / f"benchmark_data_{subject}_{data_args['orientation_type'][0]}.pkl"
-        )
-
-        print(df_results)
-
-        """ Free orientation parameters for the benchmark """
-
-        data_args_I = {
-            "n_sensors": [50],
-            "n_times": [10],
-            "n_sources": [200],
-            "n_orient": [3],
-            "nnz": nnzs,
-            "cov_type": ["diag"],
-            "path_to_leadfield": [get_leadfield_path(subject, type="free")],
-            "orientation_type": ["free"],
-            "alpha": [0.9, 0.8, 0.7, 0.5, 0.4],  # this is actually SNR
-        }
-
-        data_args_II = {
-            "n_sensors": [50],
-            "n_times": [10],
-            "n_sources": [200],
-            "n_orient": [3],
-            "nnz": nnzs,
-            "cov_type": ["full"],
-            "path_to_leadfield": [get_leadfield_path(subject, type="free")],
-            "orientation_type": ["free"],
-            "alpha": [0.9, 0.8, 0.7, 0.5, 0.4],  # this is actually SNR
-        }
-
-        
-        estimators = [
-            # (iterative_L1, data_args_I, {"alpha": estimator_alphas}, {}),
-            # (iterative_L2, data_args_I, {"alpha": estimator_alphas}, {}),
-            # (iterative_sqrt, data_args_I, {"alpha": estimator_alphas}, {}),
-            # (iterative_L1_typeII, data_args_II, {"alpha": estimator_alphas}, {}),
-            # (iterative_L2_typeII, data_args_II, {"alpha": estimator_alphas}, {}),
-            (gamma_map, data_args_II, {"alpha": estimator_alphas},  {"update_mode": 1}),
-            (gamma_map, data_args_II, {"alpha": estimator_alphas},  {"update_mode": 2}),
-            (gamma_map, data_args_II, {"alpha": estimator_alphas},  {"update_mode": 3}),
-        ]
-
-        df_results = []
-        for estimator, data_args, estimator_args, estimator_extra_params in estimators:
-            benchmark = Benchmark(
-                estimator,
-                subject,
-                metrics,
-                data_args,
-                estimator_args,
-                random_state=42,
-                memory=memory,
-                n_jobs=n_jobs,
-                do_spatial_cv=do_spatial_cv,
-                estimator_extra_params=estimator_extra_params
-            )
-            results = benchmark.run(nruns=10)
-            df_results.append(results)
-
-        df_results = pd.concat(df_results, axis=0)
-
-        data_path = Path("bsi_zoo/data")
-        data_path.mkdir(exist_ok=True)
-        if do_spatial_cv:
-            FILE_NAME = (
-                f"benchmark_data_{subject}_{data_args['orientation_type'][0]}.pkl"
-            )
-        else:
-            FILE_NAME = (
-                f"benchmark_data_{subject}_{data_args['orientation_type'][0]}.pkl"
-            )
-        df_results.to_pickle(data_path / FILE_NAME)
-
-        print(df_results)
